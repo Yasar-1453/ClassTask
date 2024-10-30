c4c31e30bbd03ea02a0449ac83e999da1676774a
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-30 13:15:54 +0400

    bitirilib

---
+++ Custom List/CustomList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_List
+{
+    internal class CustomList<T>
+    {
+        T[] List = new T[0];
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+
+
+        public void Add(T item)
+        {
+            if (Capacity == 0)
+            {
+                Capacity += 4;
+                Array.Resize(ref List, List.Length + 4);
+                List[Count] = item;
+            }
+            else if(Capacity == Count)
+            {
+                Capacity *= 2;
+                Array.Resize(ref List, List.Length * 2);
+                List[Count] = item;
+
+            }
+            else
+            {
+                List[Count] = item;
+            }
+            Count++;
+        }
+      
+
+        public CustomList<T> FindAllCustom(Predicate<T> predicate)
+        {
+            CustomList<T> results = new CustomList<T>();
+            foreach (T item in List)
+            {
+                if (predicate(item))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        public T FindCustom(Predicate<T> predicate)
+        {
+            foreach (T item in List)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+
+        public T FirstOrDefault(Func<T, bool> predicate)
+        {
+            foreach (T item in List)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return List[i];
+            }
+        }
+        
+    }
+}
---
+++ Custom List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<string> myList = new CustomList<string>();
+
+            myList.Add("1");
+            myList.Add("2");
+            myList.Add("3");
+            myList.Add("4");
+            myList.Add("5");
+
+            myList = myList.FindAllCustom(x => x == "6");
+
+            foreach (var item in myList) 
+            {
+
+                Console.WriteLine(item);
+            }
+
+
+
+            List<string> yourList = new List<string>();
+
+            yourList.Add("1");
+            yourList.Add("2");
+            yourList.Add("3");
+            yourList.Add("4");
+            yourList.Add("5");
+
+
+           
+
+
+
+        }
+    }
+}
